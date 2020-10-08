29cecf9b552b6354f168ac8ec169b985d6597b33
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-10-08 22:44:38 +0100

    dev deps

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+const doAndAdd = func => files => [
+  ...files.map(func),
+  ...files.map(file => `git add ${file}`)
+]
+
+module.exports = {
+  '*.package.json': doAndAdd(file => `sort-package-json ${file}`),
+  '*.ts': doAndAdd(file => `eslint ${file} --fix`)
+}
